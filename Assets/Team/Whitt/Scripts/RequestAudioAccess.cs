2dfea1d36f96106f1c117736873f28cac1ef9ab3
WhittSellers <44481407+WhittSellers@users.noreply.github.com>
2022-03-25 20:16:17 -0700

    worked on reactive shader and audio source selection and mic permissions

--- Assets/Team/Whitt/Scripts/RequestAudioAccess.cs
+++ Assets/Team/Whitt/Scripts/RequestAudioAccess.cs
@@ -1,59 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using System.Threading.Tasks;
 
-public class SelectAudioSource : MonoBehaviour
+public class AudioCapturePermissions
 {
-    AudioClip _clip = null;
-
-    public AudioClip demoSong;
-
-    AudioSource _source = null;
-
-    public bool useMic = false;
-
-    public bool showStartUI = true;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        _source = GetComponent<AudioSource>();
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        SetAudioSource();
-    }
-
-    public void SetAudioSource()
-    {
-        if (showStartUI == false && useMic == true)
-        {
-            _clip = Microphone.Start("Built-In Microphone", true, 10, 44100);
-            _source.loop = true;
-            
-            while(!(Microphone.GetPosition(null) > 0)) { }
-            _source.Play();
-        }
-
-        if (showStartUI == false && useMic == false)
-        {
-            _clip = demoSong;
-            _source.loop = true;
-            _source.Play();
-        }
-    }
-
-    public void DeactivateAudio()
-    {
-        _source.Stop();
-        if (useMic)
-            Microphone.End("Built-In Microphone");
-        _clip = null;
-        showStartUI = true;
-    }
-
+    // If no microphone is present, an exception is thrown with the following HResult value.
     private static int NoCaptureDevicesHResult = -1072845856;
 
     /// <summary>
